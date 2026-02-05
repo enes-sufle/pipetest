936d95700457af9c2d7bc7dca5b569a58e21d4ac
Enes Cakmak <enes@sufle.io>
2026-02-05 14:52:18 +0300

    Initial commit

---
+++ src/main/java/com/example/pipetest/PipetestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pipetest/PipetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
