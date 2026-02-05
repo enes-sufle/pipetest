e3ef465b0425ffcaa40ddcbc872dc469bef097cd
Enes Cakmak <enes@sufle.io>
2026-02-05 15:25:32 +0300

    controller created

---
+++ src/main/java/com/example/pipetest/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.pipetest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Pipeline Calisiyor! - Enes";
+    }
+}
